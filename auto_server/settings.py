46987783fb6620341fbd59ebdcb107be3db5f87f
Robert <lichunjin521@gmail.com>
2017-10-11 16:51:14 +0800

    配置引入静态文件路径STATICFILES_DIRS

--- auto_server/settings.py
+++ auto_server/settings.py
@@ -141,8 +141,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-# 物理路径
-# STATIC_ROOT = (os.path.join(BASE_DIR, "app01/static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 
 # 配置media
